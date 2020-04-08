002210b6872db436cb1e10a6ebbcaae87cc0dae4
Weskley Bezerra <weskleymb@hotmail.com>
2020-04-08 15:53:57 -0300

    aula 02

--- src/main/java/br/senai/br/ti/agenda/controller/ContatoController.java
+++ src/main/java/br/senai/br/ti/agenda/controller/ContatoController.java
@@ -1,4 +1,19 @@
 package br.senai.br.ti.agenda.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("agenda")
 public class ContatoController {
+
+    @GetMapping
+    public String home(Model model) {
+        String nome = "Weskley";
+        model.addAttribute("nome", nome);
+        return "index";
+    }
+
 }
