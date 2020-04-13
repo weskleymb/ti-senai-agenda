9ed3337ad3433f2eeb5e2a5ecea3df8b219cb2aa
Weskley Bezerra <weskleymb@hotmail.com>
2020-04-13 16:16:07 -0300

    melhorias no codigo

--- src/main/java/br/senai/br/ti/agenda/controller/ContatoController.java
+++ src/main/java/br/senai/br/ti/agenda/controller/ContatoController.java
@@ -7,12 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("agenda")
 public class ContatoController {
 
     @Autowired
