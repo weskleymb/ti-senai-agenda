f760bf4640a67dde738eef22b0cb8cc7f9585470
Weskley Bezerra <weskleymb@hotmail.com>
2020-04-16 13:17:18 -0300

    melhorias no codigo

--- src/main/java/br/senai/br/ti/agenda/AgendaApplication.java
+++ src/main/java/br/senai/br/ti/agenda/AgendaApplication.java
@@ -1,13 +1,16 @@
 package br.senai.br.ti.agenda;
 
+import br.senai.br.ti.agenda.model.Contato;
 import br.senai.br.ti.agenda.repository.ContatoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
-public class AgendaApplication implements CommandLineRunner {
+public class AgendaApplication {
 
     @Autowired
     private ContatoRepository repository;
@@ -16,17 +19,23 @@ public class AgendaApplication implements CommandLineRunner {
         SpringApplication.run(AgendaApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-//		Contato contato1 = new Contato();
-//		contato1.setNome("Jose");
-//		contato1.setFone("(11) 2222-3333");
-//		repository.save(contato1);
-//
-//		Contato contato2 = new Contato();
-//		contato2.setNome("Pedro");
-//		contato2.setFone("(33) 98888-0000");
-//		repository.save(contato2);
+    @Bean
+    @Profile("dev")
+    public ApplicationRunner devApplicationRunner(){
+        return arg->{
+            try {
+                Contato contato1 = new Contato();
+                contato1.setNome("Jose");
+                contato1.setFone("(11) 2222-3333");
+                repository.save(contato1);
+                Contato contato2 = new Contato();
+                contato2.setNome("Pedro");
+                contato2.setFone("(33) 98888-0000");
+                repository.save(contato2);
+            } catch (Exception e) {
+                System.out.println("-- FALHA NA INCLUSÃO --");
+            }
+        };
     }
 
 }
